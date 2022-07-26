eaff8ce91bd22d284a30abd0b301aba79e6492d4
nahee-park <nahee.developer@gmail.com>
2022-07-26 16:26:50 +0900

    Revert "chore: solana wallet module 설치"
    This reverts commit db1c9e643b7b84dc031fe38ae5c55fdb90501563.

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,32 +1,27 @@
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
-import Main from '@src/components/Main';
+import Head from 'next/head';
 
-const endPoint = 'http://127.0.0.1:8899';
-
-const Home: NextPage = () => {
-  const wallets = [getPhantomWallet()];
+const Feed: NextPage = () => {
   return (
-    <ConnectionProvider endpoint={endPoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <Styled.Page>
-          <Main />
-        </Styled.Page>
-      </WalletProvider>
-    </ConnectionProvider>
+    <Styled.Page>
+      <Head>
+        <title>SOPTstagram</title>
+        <meta name="description" content="SOPTstagram" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      메인입니다
+    </Styled.Page>
   );
 };
 
-export default Home;
+export default Feed;
 
 const Styled = {
   Page: styled.div`
     width: 100vw;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    & > * {
+      width: 100%;
+    }
   `,
 };
