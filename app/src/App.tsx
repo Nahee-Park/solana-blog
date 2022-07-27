5f43954f2d8a020bb7f50c5890fd59dd62dd356f
nahee-park <nahee.developer@gmail.com>
2022-07-27 14:23:19 +0900

    dev: 세팅추가

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,42 @@
+import React, { FC, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
-import Router from './core/router';
-
-const wallets = [getPhantomWallet()];
-const endPoint = "http://127.0.0.1:8899";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  CoinbaseWalletAdapter,
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  createDefaultAuthorizationResultCache,
+  SolanaMobileWalletAdapter,
+} from "@solana-mobile/wallet-adapter-mobile";
+import Router from "./core/router";
 
 const App = () => {
+  // const wallets = [getPhantomWallet()];
+  const endPoint = "http://127.0.0.1:8899";
+  const network = WalletAdapterNetwork.Devnet;
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+
   return (
     <ConnectionProvider endpoint={endPoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <Router/>
+        <Router />
+        여기 들어가니
       </WalletProvider>
     </ConnectionProvider>
   );
