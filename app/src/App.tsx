7250a03048cef776f80ff47f31ec1f0b64606f3b
nahee-park <nahee.developer@gmail.com>
2022-07-28 10:38:59 +0900

    feat: json-rpc예제 추가'

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,14 @@
-import React, { FC, useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  CoinbaseWalletAdapter,
-  GlowWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import {
-  createDefaultAuthorizationResultCache,
-  SolanaMobileWalletAdapter,
-} from "@solana-mobile/wallet-adapter-mobile";
-import Router from "./core/router";
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import Router from './core/router';
 
 const App = () => {
   // const wallets = [getPhantomWallet()];
-  const endPoint = "http://127.0.0.1:8899";
+  const endPoint = 'http://127.0.0.1:8899';
   const network = WalletAdapterNetwork.Devnet;
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
 
@@ -36,7 +16,6 @@ const App = () => {
     <ConnectionProvider endpoint={endPoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <Router />
-        여기 들어가니
       </WalletProvider>
     </ConnectionProvider>
   );
--- app/src/component/Home.tsx
+++ app/src/component/Home.tsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { PhantomWalletName } from '@solana/wallet-adapter-phantom';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [solBalance, setSolBalance] = useState<number>();
+  const { connection } = useConnection();
+  const { connected, publicKey, sendTransaction, select } = useWallet();
+
+  const getTokenAccountByOwner = async () => {
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8899',
+        {
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'getTokenAccountsByOwner',
+          params: [
+            '4Qkev8aNZcqFNSRhQzwyLMFSsi94jHqE8WNVTJzTP99F',
+            {
+              mint: '3wyAj7Rt1TWVPZVteFJPLa26JmLvdb1CAKEFZm3NY75E',
+            },
+            {
+              encoding: 'jsonParsed',
+            },
+          ],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        },
+      );
+      console.log('>>>>getTokenAccountByOwner', response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getBalance = async () => {
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8899',
+        {
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'getBalance',
+          params: ['5hPeeELRbsUtq8ZEXMY7SLr9WfSGipGrfSGBgPRKnj5'],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        },
+      );
+      console.log('>>>>getBalance', response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSendTransaction = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: Keypair.generate().publicKey,
+        lamports: 1,
+      }),
+    );
+
+    const signature = await sendTransaction(transaction, connection);
+
+    await connection.confirmTransaction(signature, 'processed');
+  }, [publicKey, sendTransaction, connection]);
+
+  const handleConnect = () => {
+    select(PhantomWalletName);
+    console.log('>>publickKet', publicKey);
+    console.log('connected', connected);
+    console.log('>>connection본래정보', connection);
+  };
+
+  useEffect(() => {
+    getTokenAccountByOwner();
+    getBalance();
+  }, []);
+
+  const handleBalanceClick = async () => {
+    if (!publicKey) {
+      return;
+    }
+    let balance = await connection.getBalance(publicKey);
+    console.log(`${balance / LAMPORTS_PER_SOL} SOL`);
+    setSolBalance(balance / LAMPORTS_PER_SOL);
+  };
+
+  const handleGetAllToken = async () => {
+    if (!publicKey) {
+      return;
+    }
+  };
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <button onClick={handleConnect}>connect to wallet</button>
+      <button onClick={handleBalanceClick} disabled={!publicKey}>
+        get balance
+      </button>
+      <button onClick={handleGetAllToken} disabled>
+        get all token from wallet address
+      </button>
+      {solBalance && <h1>{solBalance} sol</h1>}
+    </>
+  );
 }
 
-export default Home
+export default Home;
