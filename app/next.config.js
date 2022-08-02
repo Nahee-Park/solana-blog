2c1e5b5e6d3f8de613a6f52054a7d77262bf1ab7
nahee-park <nahee.developer@gmail.com>
2022-08-02 17:40:31 +0900

    feat: 폴리필 설정

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      'crypto-browserify': require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify
+      'stream-browserify': require.resolve('stream-browserify'),
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
